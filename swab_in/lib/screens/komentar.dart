b92aeece62c5d7460467264c01dfe90f79891f6a
Umar Izzuddin <umar.izzuddin@ui.ac.id>
2021-12-29 22:59:37 +0700

    init komentar

--- swab_in/lib/screens/komentar.dart
+++ swab_in/lib/screens/komentar.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class KomentarState extends StatefulWidget {
   const KomentarState({Key? key, required this.title}) : super(key: key);
@@ -9,11 +13,53 @@ class KomentarState extends StatefulWidget {
   State<KomentarState> createState() => KomentarScreen();
 }
 
-class KomentarScreen extends State<KomentarState> {
+class Forum {
+  final String title;
+  final String imageUrl;
 
+  Forum({required this.title, required this.imageUrl});
+
+  factory Forum.fromJson(Map<String, dynamic> json) {
+    return Forum(
+      title: json['title'],
+      imageUrl: json['image'],
+    );
+  }
+}
+
+List<Forum> parseForum(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Forum>((json) => Forum.fromJson(json)).toList();
+}
+
+Future<List<Forum>> fetchForum() async {
+  final response =
+      await http.get(Uri.parse('http://10.0.2.2:8000/forum/forum_content'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return compute(parseForum, response.body);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Forum');
+  }
+}
+
+class KomentarScreen extends State<KomentarState> {
   final _formKey = GlobalKey<FormState>();
 
+  late Future<List<Forum>> futureForum;
+
   TextEditingController komentarController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    futureForum = fetchForum();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,9 +110,18 @@ class KomentarScreen extends State<KomentarState> {
                             alignment: Alignment.bottomRight,
                             margin: EdgeInsets.only(top: 10, bottom: 10),
                             child: ElevatedButton(
-                                onPressed: () {
-                                  print(komentarController.text);
-
+                                onPressed: () async {
+                                  await http.post(
+                                    Uri.parse(
+                                        'http://10.0.2.2:8000/forum/komentar_post'),
+                                    headers: <String, String>{
+                                      'Content-Type':
+                                          'application/json; charset=UTF-8',
+                                    },
+                                    body: jsonEncode(<String, String>{
+                                      'title': komentarController.text,
+                                    }),
+                                  );
                                 },
                                 child: Container(
                                   padding: EdgeInsets.fromLTRB(5, 7, 5, 7),
@@ -104,23 +159,20 @@ class KomentarScreen extends State<KomentarState> {
                             Text("Bener banget"),
                           ]),
                       SizedBox(height: 15),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Swabdev",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500)),
-                            Text("Bener banget 2"),
-                          ]),
-                      SizedBox(height: 15),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Swabdev",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500)),
-                            Text("Bener banget 3"),
-                          ]),
+                      FutureBuilder<List<Forum>>(
+                        future: futureForum,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Image.network(snapshot.data![0].imageUrl);
+                          } else if (snapshot.hasError) {
+                            return Text('${snapshot.error}');
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+
+                          // By default, show a loading spinner.
+                        },
+                      )
                     ],
                   ),
                 ),
