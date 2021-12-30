f286356406a05bbcb316a82ffef41d156be114d2
Umar Izzuddin <umar.izzuddin@ui.ac.id>
2021-12-31 04:02:05 +0700

    more on fetching comments

--- swab_in/lib/screens/komentar.dart
+++ swab_in/lib/screens/komentar.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import "string_extension.dart";
 
 class KomentarState extends StatefulWidget {
   const KomentarState({Key? key, required this.title}) : super(key: key);
@@ -13,17 +13,56 @@ class KomentarState extends StatefulWidget {
   State<KomentarState> createState() => KomentarScreen();
 }
 
+class Komentar {
+  final String komentar;
+  final String userId;
+
+  Komentar({required this.komentar, required this.userId});
+
+  factory Komentar.fromJson(Map<String, dynamic> json) {
+    return Komentar(komentar: json['komentar'], userId: json['user_id']);
+  }
+}
+
+List<Komentar> parseKomentar(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Komentar>((json) => Komentar.fromJson(json)).toList();
+}
+
+Future<List<Komentar>> fetchKomentar() async {
+  final response =
+      await http.get(Uri.parse('http://10.0.2.2:8000/forum/get_komentar'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return compute(parseKomentar, response.body);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Komentar');
+  }
+}
+
 class Forum {
   final String title;
   final String imageUrl;
+  final String writer;
+  final String content;
 
-  Forum({required this.title, required this.imageUrl});
+  Forum(
+      {required this.title,
+      required this.imageUrl,
+      required this.content,
+      required this.writer});
 
   factory Forum.fromJson(Map<String, dynamic> json) {
     return Forum(
-      title: json['title'],
-      imageUrl: json['image'],
-    );
+        title: json['title'],
+        imageUrl: json['image'],
+        content: json['content'],
+        writer: json['writer']);
   }
 }
 
@@ -50,8 +89,9 @@ Future<List<Forum>> fetchForum() async {
 
 class KomentarScreen extends State<KomentarState> {
   final _formKey = GlobalKey<FormState>();
-
+  List<Widget> listW = [];
   late Future<List<Forum>> futureForum;
+  late Future<List<Komentar>> futureKomentar;
 
   TextEditingController komentarController = TextEditingController();
 
@@ -59,6 +99,7 @@ class KomentarScreen extends State<KomentarState> {
   void initState() {
     super.initState();
     futureForum = fetchForum();
+    futureKomentar = fetchKomentar();
   }
 
   @override
@@ -91,8 +132,8 @@ class KomentarScreen extends State<KomentarState> {
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 25, bottom: 5),
-                  child: Text("Review Swab",
+                  margin: const EdgeInsets.only(top: 25, bottom: 5),
+                  child: const Text("Review Swab",
                       style:
                           TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
                 ),
@@ -101,12 +142,38 @@ class KomentarScreen extends State<KomentarState> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      const Text("Ditulis oleh: ejet",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w600)),
-                      SizedBox(height: 10),
-                      const Text(
-                          "Awalnya waktu saya mau memilih tempat swab, saya pilih tes swab di Yos Sudarso karena dekat dari rumah. Ternyata pelayanannya bagus dan hasil swabnya juga cepat keluar"),
+                      FutureBuilder<List<Forum>>(
+                        future: futureForum,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Text(snapshot.data![0].content);
+                          } else if (snapshot.hasError) {
+                            return Text('${snapshot.error}');
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+
+                          // By default, show a loading spinner.
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      FutureBuilder<List<Forum>>(
+                        future: futureForum,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            String writer = snapshot.data![0].writer.capitalize();
+                            return Text("Ditulis oleh: $writer",
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600));
+                          } else if (snapshot.hasError) {
+                            return Text('${snapshot.error}');
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+
+                          // By default, show a loading spinner.
+                        },
+                      ),
                     ],
                   ),
                 ),
@@ -117,14 +184,14 @@ class KomentarScreen extends State<KomentarState> {
                       children: [
                         TextFormField(
                             controller: komentarController,
-                            decoration: new InputDecoration(
+                            decoration: const InputDecoration(
                                 hintText: "Masukkan komentar anda")),
                         Container(
                             alignment: Alignment.bottomRight,
-                            margin: EdgeInsets.only(top: 10, bottom: 10),
+                            margin: const EdgeInsets.only(top: 10, bottom: 10),
                             child: ElevatedButton(
                                 onPressed: () async {
-                                  final response = await http.post(
+                                  await http.post(
                                     Uri.parse(
                                         'http://10.0.2.2:8000/forum/komentar_post'),
                                     headers: <String, String>{
@@ -137,10 +204,10 @@ class KomentarScreen extends State<KomentarState> {
                                       'userId': 1
                                     }),
                                   );
-                                  print(response.body);
                                 },
                                 child: Container(
-                                  padding: EdgeInsets.fromLTRB(5, 7, 5, 7),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(5, 7, 5, 7),
                                   child: const Text(
                                     "Reply",
                                     style: TextStyle(
@@ -160,21 +227,50 @@ class KomentarScreen extends State<KomentarState> {
                 ),
                 Container(
                   width: double.infinity,
-                  color: Color.fromRGBO(237, 253, 255, 1),
-                  margin: EdgeInsets.all(15),
-                  padding: EdgeInsets.all(10),
+                  color: const Color.fromRGBO(237, 253, 255, 1),
+                  margin: const EdgeInsets.all(15),
+                  padding: const EdgeInsets.all(10),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Swabdev",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500)),
-                            Text("Bener banget"),
-                          ]),
-                      SizedBox(height: 15),
+                      FutureBuilder<List<Komentar>>(
+                        future: futureKomentar,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            var list = <Widget>[];
+                            for (int i = 0; i < snapshot.data!.length; i++) {
+                              list.add(Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      snapshot.data![i].userId.capitalize(),
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(snapshot.data![i].komentar),
+                                  ],
+                                ),
+                                padding:
+                                    const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                              ));
+                            }
+                            return Column(
+                              children: list,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            );
+                          } else if (snapshot.hasError) {
+                            return Text('${snapshot.error}');
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+
+                          // By default, show a loading spinner.
+                        },
+                      ),
                     ],
                   ),
                 ),
